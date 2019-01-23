56859f5a8ae18e00761676796178868d677f5fa6
Ben Haugen <ben1232jazz@hotmail.com>
2019-01-23 21:38:13 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,7 +4,7 @@ def oxford_comma(array)
     return empty_string
   elsif array.length == 2 
     return array.join(" and ")
-  else if array > 3
+  else if array.length >= 3
     return array[0..-2].join(", ") + " and " + array[-1]
 end
 end 
