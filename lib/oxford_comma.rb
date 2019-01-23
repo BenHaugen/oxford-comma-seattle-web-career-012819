dbf2caecc1ac5b04d4806d28bbacf339cd0c4692
Ben Haugen <ben1232jazz@hotmail.com>
2019-01-23 21:40:52 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -5,7 +5,7 @@ def oxford_comma(array)
   elsif array.length == 2 
     return array.join(" and ")
   else if array.length >= 3
-    return array[0..-1].join(", ") + " and "
+    return array[0..-1].join(", ") + " and " + array[-1]
 end
 end 
 end
